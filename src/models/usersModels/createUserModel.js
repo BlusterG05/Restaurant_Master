aa551b6b4736b03b1cfeff1484c7b51baacd2783
BlusterG05 <jerycohe05@gmail.com>
2024-07-25 00:01:33 -0500

    'Register_user'

--- src/models/usersModels/createUserModel.js
+++ src/models/usersModels/createUserModel.js
@@ -1,22 +1,9 @@
 const db = require("../../db/config");
+const bcrypt = require('bcrypt');
 
-const createUser = (user, callback) => {
-  const {
-    user_ced,
-    user_name,
-    user_phone,
-    user_address,
-    user_mail,
-    user_plan,
-    city_id,
-    user_profile_pic_url,
-    password,
-  } = user;
-  const query = `CALL create_user(?, ?, ?, ?, ?, ?, ?, ?, ?)`;
-
-  db.query(
-    query,
-    [
+const createUser = async (user, callback) => {
+  try {
+    const {
       user_ced,
       user_name,
       user_phone,
@@ -26,14 +13,35 @@ const createUser = (user, callback) => {
       city_id,
       user_profile_pic_url,
       password,
-    ],
-    (err, results) => {
-      if (err) {
-        return callback(err);
+    } = user;
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const query = `CALL create_user(?, ?, ?, ?, ?, ?, ?, ?, ?)`;
+    const defaultPlan = 'Freemium'
+    db.query(
+      query,
+      [
+        user_ced,
+        user_name,
+        user_phone,
+        user_address,
+        user_mail,
+        defaultPlan,
+        city_id,
+        user_profile_pic_url,
+        hashedPassword,
+      ],
+      (err, results) => {
+        if (err) {
+          return callback(err);
+        }
+        callback(null, results[0]);
       }
-      callback(null, results[0]);
-    }
-  );
+    );
+  } catch (error) {
+    callback(error);
+  }
 };
 
 module.exports = createUser;
