8de05a65c72c0e186c43919f195baf6c2444f5f7
BlusterG05 <jerycohe05@gmail.com>
2024-07-27 01:03:49 -0500

    Public_and_private_routes

--- src/routes.js
+++ src/routes.js
@@ -11,6 +11,8 @@ const createBusinessRoute = require("./routes/businessesRoutes/createBussinessRo
 const editBusinessRoute = require("./routes/businessesRoutes/editBussinessRoute");
 const deleteBusinessRoute = require("./routes/businessesRoutes/deleteBussinessRoute");
 const getBusinessesRoute = require("./routes/businessesRoutes/getBussinessesRoute");
+const getBusinessesperUserRoute = require("./routes/businessesRoutes/getBusinessesperUserRoute");
+const getBusinessRoute = require('./routes/businessesRoutes/getBusinessRoute')
 
 const createProductRoute = require("./routes/productsRoutes/createProductRoute");
 const editProductRoute = require("./routes/productsRoutes/editProductRoute");
@@ -24,27 +26,33 @@ const loginRoute = require("./routes/usersRoutes/loginRoute")
 
 const authenticateUser = require('./middlewares/authenticateUsers')
 
-router.use("/v1",loginRoute);
+router.use("/v1", loginRoute);
 
 router.use("/v1", createUserRoute);
-router.use("/v1", editUserRoute);
-router.use("/v1", deleteUserRoute);
-router.use("/v1",authenticateUser, getUsersRoute);
-router.use("/v1", getUserRoute);
 
-
-router.use("/v1", createBusinessRoute);
-router.use("/v1", editBusinessRoute);
-router.use("/v1", deleteBusinessRoute);
 router.use("/v1", getBusinessesRoute);
 
-router.use("/v1", createProductRoute);
-router.use("/v1", editProductRoute);
-router.use("/v1", deleteProductRoute);
 router.use("/v1", getProductsRoute);
 
-router.use("/v1", createMenuRoute);
-router.use("/v1", addProductToMenuRoute);
+
+router.use("/v1", authenticateUser, editUserRoute);
+router.use("/v1", authenticateUser, deleteUserRoute);
+router.use("/v1", authenticateUser, getUsersRoute);
+router.use("/v1", authenticateUser, getUserRoute);
+
+
+router.use("/v1", authenticateUser, createBusinessRoute);
+router.use("/v1", authenticateUser, editBusinessRoute);
+router.use("/v1", authenticateUser, deleteBusinessRoute);
+router.use("/v1", authenticateUser, getBusinessesperUserRoute);
+router.use("/v1", authenticateUser, getBusinessRoute)
+
+router.use("/v1", authenticateUser, createProductRoute);
+router.use("/v1", authenticateUser, editProductRoute);
+router.use("/v1", authenticateUser, deleteProductRoute);
+
+router.use("/v1", authenticateUser, createMenuRoute);
+router.use("/v1", authenticateUser, addProductToMenuRoute);
 
 
 module.exports = router;
